ccb8878f52e24983b02ed284bdc0083583e3753a
permalik <permalik@protonmail.com>
2024-03-30 19:25:46 -0500

    feat: impl server, healthcheck

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,53 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("HelloEarl"))
+const version = "0.1.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
 }
 
 func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 9000, "api server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/v0/healthcheck", app.healthcheckHandler)
 
-	log.Print("\nestablish server connection\nport: 4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", cfg.port),
+		Handler:           mux,
+		IdleTimeout:       time.Minute,
+		ReadHeaderTimeout: 10 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      30 * time.Second,
+	}
+
+	logger.Printf("\nestablish server connection\nenv: %s\naddr: %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }

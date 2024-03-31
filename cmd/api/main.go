3b3feb5afc0a57d3d7a224a9954c36b48e37a162
permalik <permalik@protonmail.com>
2024-03-31 15:41:37 -0500

    refactor: responses

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,26 +7,24 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/permalik/temp_rest_go/internal/data"
 )
 
-const version string = "0.1.0"
-
-type config struct {
-	port int
-	env  string
-}
-
 type application struct {
-	config config
+	config data.AppConfig
 	logger *log.Logger
 }
 
 func main() {
-	var cfg config
+	var cfg data.AppConfig
 
-	flag.IntVar(&cfg.port, "port", 9000, "api server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	// TODO: envvar from flag, then .env, then default
+	// TODO: if containerized, then env
+	flag.StringVar(&cfg.Env, "env", "development", "Environment(development|staging|production)")
+	flag.IntVar(&cfg.Port, "port", 9000, "api server port")
 	flag.Parse()
+	cfg.Version = "0.1.0"
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
@@ -38,12 +36,12 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", app.home)
 	mux.HandleFunc("GET /v0/healthcheck", app.healthcheck)
-	mux.HandleFunc("POST /v0/item", app.createItem)
-	mux.HandleFunc("GET /v0/items", app.readItems)
-	mux.HandleFunc("GET /v0/item/{id}", app.readItem)
+	mux.HandleFunc("POST /v0/item", app.create_item)
+	mux.HandleFunc("GET /v0/items", app.read_items)
+	mux.HandleFunc("GET /v0/item/{id}", app.read_item)
 
 	srv := &http.Server{
-		Addr:              fmt.Sprintf(":%d", cfg.port),
+		Addr:              fmt.Sprintf(":%d", cfg.Port),
 		Handler:           mux,
 		IdleTimeout:       time.Minute,
 		ReadHeaderTimeout: 10 * time.Second,
@@ -51,7 +49,7 @@ func main() {
 		WriteTimeout:      30 * time.Second,
 	}
 
-	logger.Printf("\nestablish server connection\nenv: %s\naddr: %s", cfg.env, srv.Addr)
+	logger.Printf("\nestablish server connection\nenv: %s\naddr: %s", cfg.Env, srv.Addr)
 	err := srv.ListenAndServe()
 	logger.Fatal(err)
 }
